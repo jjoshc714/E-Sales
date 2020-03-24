671c9a703d19a99db861f705ae66085e0122dce5
jjoshc714 <jjoshc714@yahoo.com>
2020-03-23 22:19:30 -0700

    clean up and upload html output

--- Econ 144 Project 1 - Ju Won Chung.R
+++ Econ 144 Project 1 - Ju Won Chung.R
@@ -1,99 +1,86 @@
-library(lattice)
-library(foreign)
-library(MASS)
-library(car)
-require(stats)
-require(stats4)
-library(KernSmooth)
-library(fastICA)
-library(cluster)
-library(leaps)
-library(mgcv)
-library(rpart)
-library(pan)
-library(mgcv)
-library(DAAG)
-library("TTR")
-library(tis)
-require("datasets")
-require(graphics)
-library("forecast")
-#install.packages("astsa")
-#require(astsa)
-library(RColorBrewer)
-library(plotrix)
-library(nlstools)
-library(seasonal)
-library(fpp2)
+library(tis)                     #load required packages
 library(forecast)
-library(dynlm)
-library(Hmisc)
 library(ggplot2)
-library(gridExtra)
+library(gridExtra) 
+library(MASS)
 
-setwd("C:\\Users\\jjoshuac714\\Desktop\\Econ 144\\Week 3")
+setwd("C:\\Users\\jjoshuac714\\Desktop\\Econ 144\\Week 3")   #set working directory
 
-#KiM: economic factors: 1.tech progress 2. population growth 3. seasonal consumption 4. recessions
+### 1. Modeling and Forecasting Trend
 
-sales_em <- read.csv("ESHOP&MAIL.csv")
-sales_em <- ts(sales_em[,2], 1992, c(2019,11), freq = 12)
-#or t <- seq(1992, 2019.833, length = length(sales_em))
+##### a) Creating time series plot
+
+sales_em <- read.csv("ESHOP&MAIL.csv")       #store csv datset as data frame "sales_em"
+sales_em <- ts(sales_em[,2], 1992, c(2019,11), freq = 12) #transform into time series
 t <- ts(sales_em, frequency = 12, start = c(1992, 01), end = c(2019, 11))
-t <- as.numeric(time(t))
+t <- as.numeric(time(t))                     #create numeric time dummy vector that corresponds to sales_em data time period
 
 plot(sales_em, main = "Retail Sales: Electronic Shopping and \nMail-Order Houses", 
-     xlab = "Time", ylab = "$ in millions")
-nberShade()
-lines(sales_em)
+     xlab = "Time", ylab = "$ in millions")  #plot time series
+nberShade()                                  #add gray recession bands
+lines(sales_em)                              #overlay plot over gray bands
 
-sales_total <- read.csv("TOTRTRADES.csv", header = FALSE)
-sales_total <- as.numeric(gsub(",","",sales_total[,1]))
+##### b) Comparison with Total Retail plot to help explain covariance stationary
 
-par(mfrow = c(2,1))
+sales_total <- read.csv("TOTRTRADES.csv", header = FALSE) 
+#store total retail trades dataset as data frame "sales_total"
+sales_total <- as.numeric(gsub(",","",sales_total[,1]))  
+#subset only sales values in column 1 and remove commas as a numeric    
+
+
+par(mfrow = c(2,1))                                   #shrink plot window size to fit two rows, one column view
 plot(sales_em, main = "Retail Sales: Electronic Shopping and \nMail-Order Houses", 
-     xlab = "Time", ylab = "$ in millions")
+     xlab = "Time", ylab = "$ in millions")           #re-plot sales_em with recession bands
 nberShade()
 lines(sales_em)
-plot(t, sales_total, type = "l", main = "Retail Sales: Total (including Food Services)",
-     xlab = "Time", ylab = "$ in millions")
+plot(t, sales_total, type = "l", main = "Retail Sales: Total (including Food Services)",                                                
+     xlab = "Time", ylab = "$ in millions")           #plot sales_total with recession bands
 nberShade()
 lines(t, sales_total, col = "navyblue")
-par(mfrow = c(1,1))
+par(mfrow = c(1,1))                                
+#convert plot windows back to single plot view
 
-ggAcf(sales_em)
-ggPacf(sales_em)
+##### c) Plotting ACF/PACF of data
 
-t_mod1 <- tslm(sales_em ~ trend)
+ggAcf(sales_em)                  #plot Acf(autocorrelation function) of series 
+ggPacf(sales_em)                 #plot Pacf (partial autocorrelation function) of series
+
+##### d) Fitting linear and non-linear models and comparing
+
+t_mod1 <- tslm(sales_em ~ trend)    #create linear model including trend dummy
 plot1 <- ggplot() + geom_line(data = as.data.frame(sales_em), aes(x = t, y = sales_em[1:335])) +
-      geom_line(data = as.data.frame(t_mod1$fit), aes(x = t, y = t_mod1$fit), col = "red", lwd = 1) +
-      ggtitle("Trend Model 1") +
-      ylab("sales_em") +
-      xlab("Time") 
+   geom_line(data = as.data.frame(t_mod1$fit), aes(x = t, y = t_mod1$fit), col = "red", lwd = 1) +
+   ggtitle("Trend Model 1") +
+   ylab("sales_em") +
+   xlab("Time")                  
+#create ggplot the model 1(t_mod1) in red over the actual data (sales_em) as "plot1"
 
-lsales_em <- log(sales_em)
-t_mod2 <- tslm(lsales_em ~ trend)
+lsales_em <- log(sales_em)             #take natural log of sales_em as "lsales_em"
+t_mod2 <- tslm(lsales_em ~ trend)      #create natural log model including trend
 plot2 <- ggplot() + geom_line(data = as.data.frame(sales_em), aes(x = t, y = lsales_em[1:335])) +
-      geom_line(data = as.data.frame(t_mod2$fit), aes(x = t, y = t_mod2$fit), col = "red", lwd = 1) +
-      ggtitle("Trend Model 2") +
-      ylab("ln(sales_em)") +
-      xlab("Time")
+   geom_line(data = as.data.frame(t_mod2$fit), aes(x = t, y = t_mod2$fit), col = "red", lwd = 1) +
+   ggtitle("Trend Model 2") +
+   ylab("ln(sales_em)") +
+   xlab("Time")                     
+#create ggplot the model 2(t_mod2) in red over the actual data (sales_em) as "plot2"
 
-grid.arrange(plot1, plot2, nrow = 2)
+grid.arrange(plot1, plot2, nrow = 2)   
+#arrange 2 rows in a single window to plot and visually compare both models
 
-#afterwards plot with decomposed trend
+##### e) Plotting residuals vs fitted values for both trend models
 
 plot3 <- ggplot() + geom_point(aes(x = as.numeric(t_mod1$fit), y = as.numeric(t_mod1$res))) +
-      geom_line(aes(x = as.numeric(t_mod1$fit), y = 0), col = "red") +
-      geom_line(aes(x = 0, y = t_mod1$res), col = "red") +
-      ggtitle("Trend Model 1") +
-      xlab("Fitted Values") +
-      ylab("Residuals")
+   geom_line(aes(x = as.numeric(t_mod1$fit), y = 0), col = "red") +
+   geom_line(aes(x = 0, y = t_mod1$res), col = "red") +
+   ggtitle("Trend Model 1") +
+   xlab("Fitted Values") +
+   ylab("Residuals")
 plot3
 plot4 <- ggplot() + geom_point(aes(x = as.numeric(t_mod2$fit), y = as.numeric(t_mod2$res))) +
-      geom_line(aes(x = as.numeric(t_mod2$fit), y = 0),col = "red") +
-      ggtitle("Trend Model 2") +
-      xlab("Fitted Values") +
-      ylab("Residuals")
+   geom_line(aes(x = as.numeric(t_mod2$fit), y = 0),col = "red") +
+   ggtitle("Trend Model 2") +
+   xlab("Fitted Values") +
+   ylab("Residuals")
 
 plot4
 plot4a <- ggplot() + geom_point(aes(x = as.numeric(t_mod2$fit), y = as.numeric(t_mod2$res))) +
@@ -110,104 +97,112 @@ nberShade()
 lines(lsales_em, col = "black")
 abline(h = 9, col = "navyblue", lty = "dotted")
 abline(h = 10, col = "navyblue", lty = "dotted")
-#draw lines @ y = 9, and 10 for both graphs in comparison
 
-truehist(t_mod1$res, col = "skyblue", xlab = "Residuals", ylab = "Fraction",
-         main = "Trend Model 1 \nResiduals Histogram", xlim = c(-20000, 20000))
-xr <- rnorm(1000000, mean(t_mod1$res), sd(t_mod1$res))
-lines(density(xr), col = "red", lwd = 2)
-lines(density(t_mod1$res), col = "darkblue", lwd = 2)
+##### f) Plotting Residual histograms for both models
+
+truehist(t_mod1$res, col = "skyblue", xlab = "Residuals", ylab = "Fraction",  
+         main = "Trend Model 1 \nResiduals Histogram", xlim = c(-20000, 20000)) #plot histogram of t_mod1 
+xr <- rnorm(1000000, mean(t_mod1$res), sd(t_mod1$res))  
+#create a sample normal distribution from 1 mil. random values about t_mod1's residual mean and std deviation as "xr"
+lines(density(xr), col = "red", lwd = 2)                #plot "xr" line
+lines(density(t_mod1$res), col = "darkblue", lwd = 2)   #plot density distribution of t_mod1 residuals
 legend(x = "topright", legend = c("Density", "Normal Distribution"), 
-       col = c("darkblue", "red"), lty = 1, lwd = 2, bty = "n")
+       col = c("darkblue", "red"), lty = 1, lwd = 2, bty = "n")  
+#create legend for both distribution lines with corresponding color scheme
 
 truehist(t_mod2$res, col = "skyblue", xlab = "Residuals", ylab = "Fraction",
-         main = "Trend Model 2 \nResiduals Histogram", xlim = c(-0.6, 0.6))
-xr <- rnorm(1000000, mean(t_mod2$res), sd(t_mod2$res))
-lines(density(xr), col = "red", lwd = 2)
-lines(density(t_mod2$res), col = "darkblue", lwd = 2)
+         main = "Trend Model 2 \nResiduals Histogram", xlim = c(-0.6, 0.6))     #plot histogram of t_mod2
+xr <- rnorm(1000000, mean(t_mod2$res), sd(t_mod2$res))  
+#create a sample normal distribution from 1 mil. random values about t_mod2's residual mean and std deviation as "xr"
+lines(density(xr), col = "red", lwd = 2)                #plot new "xr" line
+lines(density(t_mod2$res), col = "darkblue", lwd = 2)   #plot density distribution of t_mod2 residuals
 legend(x = "topright", legend = c("Density", "Normal Distribution"), 
-       col = c("darkblue", "red"), lty = 1, lwd = 2, bty = "n")
+       col = c("darkblue", "red"), lty = 1, lwd = 2, bty = "n")  
+#create legend for both distribution lines with corresponding color scheme
+
+##### g) Getting diagnostic statistics
 
 summary(t_mod1)
 
 summary(t_mod2)
 
+##### h) Getting model selection criteria AIC and BIC for both models 
+
 AIC(t_mod1, t_mod2)
 BIC(t_mod1, t_mod2)
 
-fore1 <- forecast(t_mod2, h = 60)
-plot(fore1, main = "Trend Model 2\n 5 year Ahead (h = 60) Forecast", xlab = "Time", ylab = "lsales_em")
-legend(x = "bottomright", legend = c("80% Prediction Interval", "95% Prediction Interval"), 
-       fill = c("lightsteelblue3", "gray87"), bty = "n", cex = 1)
-legend(x = "bottom", "Point Forecast", col = "blue", lty = 1, 
-       bty = "n", cex = 1)
+##### i) Using trend model 2 (t_mod2) to forecast 5 years ahead
 
-#Seasons
+fore1 <- forecast(t_mod2, h = 60)                              
+#create forecast of model 2 for 60 monthly periods (5 years) as "fore1"
+plot(fore1, main = "Trend Model 2", xlab = "Time", ylab = "lsales_em") #plot fore1
+legend(x = "bottomright", legend = c("80% Prediction Interval", "95% Prediction Interval"), 
+       fill = c("lightsteelblue3", "gray87"), bty = "n", cex = 1)    
+#create legend that corresponds to the prediction intervals
+legend(x = "bottom", "Point Forecast", col = "blue", lty = 1, lwd = 1.5, 
+       bty = "n", cex = 1)                                           
+#create legend that corresponds to the point forecast
+
+### 2. Modeling and Forecasting Seasonality
+
+##### a) Constructing seasonal aspect of lsales_em and looking at summary statistics
+
+
 ggsubseriesplot(sales_em)
 ggseasonplot(sales_em)
 
-s_mod1 <- tslm(lsales_em ~ season)
-summary(s_mod1)
-plot(s_mod1$fit, xlim = c(1992, 2020), ylab = "Seasonal Model 1", 
-     main = "Seasons 2 to 12")
+par(mfrow = c(2,1))                    #create plot window with 2 rows
+s_mod1 <- tslm(lsales_em ~ season + 0) #create model based on lsales_em including a seasonal dummy and intercept as "s_mod1"
+summary(s_mod1)                        #return summary statistics of s_mod1
+plot(s_mod1$fit, xlim = c(1992, 2020), ylab = "Seasonal Model 1",  
+     main = "Seasonal Component of Dummy Model")  #plot s_mod1 fitted values as a time series
 
-s_lsales_em <- stl(lsales_em, s.window = "periodic")
-s_lsales_em <- s_lsales_em$time.series[,1]
-plot(s_lsales_em, ylab = "Seasonal ln(sales_em)", main = "Seasonal Adjustment using\nSTL Decomposition")
+s_lsales_em <- stl(lsales_em, s.window = "periodic")  #create periodic stl(seasonal decomposition of time)
+s_lsales_em <- s_lsales_em$time.series[,1]            #subset only the seasonal component
+plot(s_lsales_em, ylab = "Seasonal ln(sales_em)", main = "Seasonal Adjustment using\nSTL Decomposition")                                #plot the stl decomposed seasonal component to compare with s_mod1
 
-s_mod0 <- tslm(lsales_em ~ season + 0) # diff w/ +0 is that adds y-int?
-summary(s_mod0)
+##### b) Plot seasonal factors
 
-par(mfrow = c(2,1))
+par(mfrow = c(2,1))                                      #create plot window with 2 rows
 plot(s_mod1$coef, type = "l", main = "Seasonal Factors", xlab = "Season",
-     ylab = "Seasonal Factors", lwd = 2, col = "forestgreen")
-hist(s_mod1$res, main = "Histogram of Residuals", col = "steelblue2", 
-     xlim = c(-2, 2), xlab = "Seasonal Model 1 Residuals")
+     ylab = "Seasonal Factors", lwd = 2, col = "forestgreen")  #plot coefficients of s_mod1
+hist(s_mod1$res, main = "Histogram of Residuals", col = "steelblue2", xlim = c(-2, 2), xlab = "Seasonal Model 1 Residuals")                                 #plot histogram of s_mod1 residuals
 
-par(mfrow = c(1,1))
-full_mod <- tslm(lsales_em ~ trend + season) 
+##### c) Incorporate both trend and seasonal dummies to create full model and plot residuals vs fitted values
+
+par(mfrow = c(1,1))                                    
+full_mod <- tslm(lsales_em ~ trend + season)           
+#create full model of lsales_em containing both trend and seasonality as "full_mod"
 plot5 <- ggplot() + geom_point(aes(x = as.numeric(full_mod$fit), y = as.numeric(full_mod$res))) +
-      geom_line(aes(x = as.numeric(full_mod$fit), y = 0), col = "red") +
-      ggtitle("Full Model") +
-      xlab("Fitted Values") +
-      ylab("Residuals")
-plot5
+   geom_line(aes(x = as.numeric(full_mod$fit), y = 0), col = "red") +
+   ggtitle("Full Model") +
+   xlab("Fitted Values") +
+   ylab("Residuals")                                #plot full_mod's residuals vs fitted values
+plot5                                                  
+
 
 grid.arrange(plot5 + ylim(-0.4, 0.4), plot4, nrow = 2)
+#plot and compare t_mod2 and full_mod for residuals vs fit
 
-summary(full_mod)
-AIC(t_mod1, t_mod2, s_mod1, s_mod0, full_mod)
-BIC(t_mod1, t_mod2, s_mod1, s_mod0, full_mod)
-accuracy(full_mod)
-accuracy(t_mod2)
-#later maybe ets or HoltWinters could improve the model?
+##### d) Summary statistics and Error metrics
 
-fore2 <- forecast(full_mod, h = 60)
-plot(fore2, main = "5 year Ahead \n(h = 60) Forecast", xlab = "Time", ylab = "lsales_em")
-plot(fore2, main = "5 year Ahead \n(h = 60) Forecast", xlab = "Time", ylab = "lsales_em",
+summary(full_mod)                       #summary statistics of full_mod
+
+AIC(t_mod1, t_mod2, s_mod1, full_mod)   #return AIC of both trend models, seasonal model, and full model
+BIC(t_mod1, t_mod2, s_mod1, full_mod)   #return BIC of both trend models, seasonal model, and full model
+
+accuracy(full_mod)                      #return error metrics of full model
+accuracy(t_mod2)                        #return error metrics of trend model 2 (the next best fit/model)
+
+##### e) Using full model (full_mod) to forecast 5 years ahead
+
+fore2 <- forecast(full_mod, h = 60)    #create forecast of full model for 60 monthly periods (5 years) as "fore2"
+plot(fore2, main = "Full Model", xlab = "Time", ylab = "lsales_em")
+plot(fore2, main = "Full Model", xlab = "Time", ylab = "lsales_em", #zoom into fore2 plot for 2017 to 2025
      xlim = c(2017, 2025))
 legend(x = "bottomright", legend = c("80% Prediction Interval", "95% Prediction Interval"), 
-       fill = c("lightsteelblue3", "gray87"), bty = "n", cex = 1.1)
+       fill = c("lightsteelblue3", "gray87"), bty = "n", cex = 1.1)  
+#create legend that accounts for the prediction intervals
 legend(x = "bottomleft", "Point Forecast", col = "blue", lty = 1,
        lwd = 2, bty = "n", cex = 1.1)
-
-
-#these are prediction intervals:
-#"Forecasts are intervals for random variables and therefore do not fit within the 
-## concept of confidence intervals, which concern parameters. You probably mean to refer 
-### to prediction intervals rather than CIs"
-
-#possible improvements: still not exactly additive, so many some smoothing/multiplicative measures
-# stltest <- stl(lsales_em , s.window = 5) & check other metrics e.g X11
-# plot(stltest$time.series[,1])
-
-###
-### NOTE: when change fullmod, and other; may need to go back to rmd and change notes 
-###       based on new info
-
-
-#KiM: add titles to all a) to whatever(explaining what is going on)
-## also, add some explaining notes to final accuracy functions for full_mod and trend mod2
-### also, check descriptions and make sure they correspond to updated info
-#### also, make sure R Source section V. corresponds to r markdown output
-#### also, in intro paragraph 2; expand more on e-shopping getting big in U.S(also source)
+#create legend that accounts for the point forecast
